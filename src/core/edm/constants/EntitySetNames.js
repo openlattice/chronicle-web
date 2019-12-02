4685eb34d98a3d228eb836f834a375872d5f7a7f
Hristo Oskov <hristo@openlattice.com>
2019-12-02 10:10:49 -0800

    EntitySet names

--- src/core/edm/constants/EntitySetNames.js
+++ src/core/edm/constants/EntitySetNames.js
@@ -2,9 +2,17 @@
  * @flow
  */
 
-const ASSOCIATION_ENTITY_SET_NAMES = {};
+const ASSOCIATION_ENTITY_SET_NAMES = {
+  PARTICIPATED_IN: 'chronicle_participated_in',
+  RECORDED_BY: 'chronicle_recorded_by',
+  USED_BY: 'chronicle_used_by',
+};
 
-const ENTITY_SET_NAMES = {};
+const ENTITY_SET_NAMES = {
+  APPLICATION_DATA: 'chronicle_app_data',
+  CHRONICLE_DEVICES: 'chronicle_device',
+  CHRONICLE_STUDIES: 'chronicle_study',
+};
 
 const ENTITY_SET_NAMES_LIST = [];
 Object.keys(ASSOCIATION_ENTITY_SET_NAMES).forEach((name :string) => {
