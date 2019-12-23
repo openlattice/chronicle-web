32745666d5f15ce6b51f07cc591666c515535c5a
Alfonce Nzioka <alfonce@openlattice.com>
2019-12-23 13:58:48 -0800

    fix tests

--- src/core/router/Routes.js
+++ src/core/router/Routes.js
@@ -3,12 +3,14 @@
  */
 const ID_PARAM :':id' = ':id';
 const ROOT :string = '/';
+const LOGIN :string = '/login';
 const STUDIES :string = '/studies';
 const STUDY :string = `${STUDIES}/${ID_PARAM}`;
 const PARTICIPANTS :string = `${STUDIES}/${ID_PARAM}/participants`;
 
 export {
   ID_PARAM,
+  LOGIN,
   PARTICIPANTS,
   ROOT,
   STUDIES,
