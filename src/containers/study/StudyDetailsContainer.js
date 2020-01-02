10053be5441f9e021c638fbd325f35ae78d220ca
Alfonce Nzioka <alfonce@openlattice.com>
2020-01-02 11:00:02 -0800

    Add tabs to study details page

--- src/containers/study/StudyDetailsContainer.js
+++ src/containers/study/StudyDetailsContainer.js
@@ -5,19 +5,25 @@
 import React from 'react';
 
 import styled from 'styled-components';
+import { faInfo, faUsers } from '@fortawesome/pro-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Map } from 'immutable';
-import { Card, CardSegment } from 'lattice-ui-kit';
+import { Card, CardSegment, Colors } from 'lattice-ui-kit';
 import { useDispatch, useSelector } from 'react-redux';
+import { Route, Switch } from 'react-router';
+import { NavLink } from 'react-router-dom';
 import type { Match } from 'react-router';
 
 import StudyDetails from './StudyDetails';
 import StudyParticipants from './StudyParticipants';
 
+import * as Routes from '../../core/router/Routes';
 import { PROPERTY_TYPE_FQNS } from '../../core/edm/constants/FullyQualifiedNames';
 import { getIdFromMatch } from '../../core/router/RouterUtils';
 import { goToRoot } from '../../core/router/RoutingActions';
 
 const { STUDY_NAME } = PROPERTY_TYPE_FQNS;
+const { PURPLES } = Colors;
 
 const StudyNameWrapper = styled.h2`
   align-items: flex-start;
@@ -31,6 +37,40 @@ const StudyNameWrapper = styled.h2`
 type Props = {
   match :Match;
 };
+
+const Tabs = styled.div`
+  border-bottom: 2px solid #eee;
+  display: flex;
+  height: 40px;
+`;
+
+const TabItem = styled.div`
+  align-items: center;
+  display: flex;
+  margin-bottom: -1px;
+  margin-right: 20px;
+`;
+
+const TabLink = styled(NavLink)`
+  border: 2px solid transparent;
+  color: rgba(66,66,66,.7);
+  font-size: 16px;
+  height: 100%;
+  outline: none;
+  padding: 0 20px;
+  text-decoration: none;
+
+  :focus {
+    border-bottom: 2px solid ${PURPLES[2]};
+    color: ${PURPLES[2]};
+    text-decoration: none;
+  }
+
+  &.active {
+    border-bottom: 2px solid ${PURPLES[2]};
+    color: ${PURPLES[2]};
+  }
+`;
 
 const StudyDetailsContainer = (props :Props) => {
   const {
@@ -52,13 +92,28 @@ const StudyDetailsContainer = (props :Props) => {
       </StudyNameWrapper>
       <Card>
         <CardSegment vertical>
-          <StudyDetails study={study} />
-          <StudyParticipants study={study} />
+          <Tabs>
+            <TabItem>
+              <TabLink exact to={Routes.STUDY.replace(Routes.ID_PARAM, studyUUID)}>
+                <FontAwesomeIcon icon={faInfo} />
+                <span style={{ marginLeft: '10px' }}> Study Details </span>
+              </TabLink>
+            </TabItem>
+            <TabItem>
+              <TabLink exact to={Routes.PARTICIPANTS.replace(Routes.ID_PARAM, studyUUID)}>
+                <FontAwesomeIcon icon={faUsers} />
+                <span style={{ marginLeft: '10px' }}> Participants </span>
+              </TabLink>
+            </TabItem>
+          </Tabs>
+          <Switch>
+            <Route path={Routes.PARTICIPANTS} render={() => <StudyParticipants study={study} />} />
+            <Route path={Routes.STUDY} render={() => <StudyDetails study={study} />} />
+          </Switch>
         </CardSegment>
       </Card>
     </>
   );
-
 };
 
 // $FlowFixMe
