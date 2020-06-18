8a576acbc978c2a2f88fa4a1d5cb1d9e98ddb858
Alfonce Nzioka <=>
2020-06-18 14:38:23 -0700

    get rruleSet from fabricate formdata

--- src/containers/questionnaires/utils/utils.js
+++ src/containers/questionnaires/utils/utils.js
@@ -2,10 +2,12 @@
 
 import { get, getIn } from 'immutable';
 import { DataProcessingUtils } from 'lattice-fabricate';
+import { RRule, RRuleSet } from 'rrule';
+import invert from 'lodash/invert';
 
 import { ENTITY_SET_NAMES } from '../../../core/edm/constants/EntitySetNames';
 import { PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
-import { QUESTIONNAIRE_SUMMARY } from '../constants/constants';
+import { QUESTIONNAIRE_SUMMARY, DAYS_OF_WEEK } from '../constants/constants';
 
 const {
   TITLE,
@@ -41,7 +43,7 @@ const getQuestionnaireSummaryFromForm = (formData :Object = {}) => {
   eak = getEntityAddressKey(0, QUESTIONNAIRE_ES_NAME, DESCRIPTION_FQN);
   result[DESCRIPTION] = getIn(formData, [psk, eak]);
 
-  psk = getPageSectionKey(1, 2);
+  psk = getPageSectionKey(2, 1);
   const questions :Object[] = get(formData, psk);
   const singleAnswerQuestions = questions.filter((question) => Object.keys(question).length === 1);
 
@@ -51,6 +53,46 @@ const getQuestionnaireSummaryFromForm = (formData :Object = {}) => {
   return result;
 };
 
+// return [hour, minute] from a 24 hour formated time string
+// input example: '21:00', '09:00'
+const parseTimeStr = (time :string) => {
+  const tokens = time.split(':');
+  return tokens.map((token) => parseInt(token, 10));
+};
+
+// return an array of RRule.MO, RRule.TU etc from ['Monday', 'Tuesday'] etc
+const getRRuleWeekDayConstants = (daysOfWeek :string[]) => {
+  const mapper = invert(DAYS_OF_WEEK); // {'Monday': 'MO', 'Tuesday': 'TU'}
+  return daysOfWeek.map((day) => RRule[get(mapper, day)]);
+};
+
+// generate recurrence rules as defined in https://tools.ietf.org/html/rfc5545
+const createRecurrenceRuleSetFromFormData = (formData :Object) => {
+  const psk = getPageSectionKey(3, 1);
+
+  const daysOfWeek :string[] = getIn(formData, [psk, 'daysOfWeek']);
+  const daysOfWeekConsts = getRRuleWeekDayConstants(daysOfWeek);
+
+  const selectedTimes :string[] = getIn(formData, [psk, 'time'])
+    .map((time :Object) => get(time, 'time')); // TODO time const
+
+  const rruleSet = new RRuleSet();
+
+  // add rules to rruleset
+  selectedTimes.forEach((time) => {
+    const [hour, minute] = parseTimeStr(time);
+
+    rruleSet.rrule(new RRule({
+      freq: RRule.WEEKLY,
+      byweekday: daysOfWeekConsts,
+      byhour: hour,
+      byminute: minute
+    }));
+  });
+  return rruleSet.toString();
+};
+
 export {
-  getQuestionnaireSummaryFromForm
+  createRecurrenceRuleSetFromFormData,
+  getQuestionnaireSummaryFromForm,
 };
