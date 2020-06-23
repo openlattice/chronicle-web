a4f8bebe615f934c4e0a1e039cf3463e103d3f24
Alfonce Nzioka <=>
2020-06-23 15:17:16 -0700

    use ActionModal component

--- src/containers/questionnaires/components/DeleteQuestionnaireModal.js
+++ src/containers/questionnaires/components/DeleteQuestionnaireModal.js
@@ -1,14 +1,19 @@
 // @flow
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
-import { Modal, ModalFooter } from 'lattice-ui-kit';
+import styled from 'styled-components';
+import { ActionModal } from 'lattice-ui-kit';
 import { useDispatch, useSelector } from 'react-redux';
 import { RequestStates } from 'redux-reqseq';
 import type { RequestState } from 'redux-reqseq';
 
 import { resetRequestState } from '../../../core/redux/ReduxActions';
 import { DELETE_QUESTIONNAIRE, deleteQuestionnaire } from '../../questionnaire/QuestionnaireActions';
+
+const Wrapper = styled.div`
+  max-width: 500px
+`;
 
 type Props = {
   isVisible :boolean;
@@ -25,61 +30,45 @@ const DeleteQuestionnaireModal = ({
 } :Props) => {
   const dispatch = useDispatch();
 
-  const [modalContentText, setModalContentText] = useState(
-    'Are you sure you want to delete questionnaire? This action cannot be reversed.'
-  );
-
   const deleteQuestionnaireRS :RequestState = useSelector(
     (state) => state.getIn(['questionnaire', DELETE_QUESTIONNAIRE, 'requestState'])
   );
-
-  useEffect(() => {
-    if (deleteQuestionnaireRS === RequestStates.FAILURE) {
-      setModalContentText('Sorry, an error occurred while attempting to delete questionnaire.');
-    }
-    if (deleteQuestionnaireRS === RequestStates.SUCCESS) {
-      setModalContentText('Questionnaire was successfully deleted.');
-    }
-  }, [deleteQuestionnaireRS]);
 
   // reset delete requestState on dismount
   useEffect(() => () => {
     dispatch(resetRequestState(DELETE_QUESTIONNAIRE));
   }, [dispatch]);
 
-  const onConfirmDelete = () => {
+  const handleDelete = () => {
     dispatch(deleteQuestionnaire({
       studyEKID, questionnaireEKID
     }));
   };
 
-  const primaryBtnText = deleteQuestionnaireRS === RequestStates.SUCCESS
-    ? undefined
-    : 'Delete';
-
-  const renderFooter = () => (
-    <ModalFooter
-        isPendingPrimary={deleteQuestionnaireRS === RequestStates.PENDING}
-        onClickPrimary={onConfirmDelete}
-        onClickSecondary={onClose}
-        textPrimary={primaryBtnText}
-        textSecondary="Cancel" />
-  );
+  const requestStateComponents = {
+    [RequestStates.STANDBY]: (
+      <Wrapper>
+        <span>Are you sure you want to delete questionnaire? This action cannot be reversed.</span>
+      </Wrapper>
+    ),
+    [RequestStates.FAILURE]: (
+      <span>Sorry, an error occurred while attempting to delete questionnaire.</span>
+    ),
+    [RequestStates.SUCCESS]: (
+      <span>Questionnaire was successfully deleted.</span>
+    )
+  };
 
   return (
-    <Modal
+    <ActionModal
         isVisible={isVisible}
+        onClickPrimary={handleDelete}
         onClose={onClose}
-        textPrimary={primaryBtnText}
+        requestState={deleteQuestionnaireRS}
+        requestStateComponents={requestStateComponents}
+        textPrimary="Delete"
         textSecondary="Cancel"
-        textTitle="Delete Questionnaire"
-        withFooter={renderFooter}>
-      <div style={{ maxWidth: '500px' }}>
-        <span>
-          { modalContentText }
-        </span>
-      </div>
-    </Modal>
+        textTitle="Delete Questionnaire" />
   );
 };
 
