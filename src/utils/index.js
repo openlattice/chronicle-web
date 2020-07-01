4553e958cc0a38bfb6f82e78888cea9066131fc9
Hristo Oskov <hristo@openlattice.com>
2020-07-01 09:37:17 -0700

    fix export

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,13 +5,11 @@
 import * as AppUtils from './AppUtils';
 import * as Errors from './Errors';
 import * as FormUtils from './FormUtils';
-import * as NotificationsUtils from './NotificationsUtils';
 import * as ParticipantUtils from './ParticipantUtils';
 
 export {
   AppUtils,
   Errors,
   FormUtils,
-  NotificationsUtils,
   ParticipantUtils,
 };
