1dc11e1d9c2f8dea2fc9fe5005876f7dcf3a4ecc
Alfonce Nzioka <alfonce.starfish@gmail.com>
2020-09-02 13:43:17 -0700

    remove unused import

--- src/core/edm/EDMUtils.js
+++ src/core/edm/EDMUtils.js
@@ -2,10 +2,9 @@
 
 import { Map } from 'immutable';
 import type { FQN } from 'lattice';
-import merge from 'lodash/merge';
 
-import { APP_REDUX_CONSTANTS } from '../../utils/constants/ReduxConstants';
 import { CHRONICLE_CORE, DATA_COLLECTION, QUESTIONNAIRES } from '../../utils/constants/AppModules';
+import { APP_REDUX_CONSTANTS } from '../../utils/constants/ReduxConstants';
 
 const { SELECTED_ORG_ID, ENTITY_SET_IDS_BY_ORG_ID } = APP_REDUX_CONSTANTS;
 
