c9489a51e8da6417b0056db92d3e179e28448ed7
Alfonce Nzioka <alfonce.starfish@gmail.com>
2021-04-05 20:23:40 +0300

    clean up jest config

--- config/jest/jest.config.js
+++ config/jest/jest.config.js
@@ -20,7 +20,7 @@ module.exports = {
     __VERSION__: PACKAGE.version,
   },
   moduleNameMapper: {
-      '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/config/jest/__mocks__/fileMock.js',
+    '\\.(png)$': '<rootDir>/config/jest/__mocks__/fileMock.js',
     '\\.(css|less)$': '<rootDir>/config/jest/__mocks__/styleMock.js'
   },
   modulePathIgnorePatterns: [
