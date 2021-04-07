6bad35fc2e47169f130d70fdfdda504dcf3360d0
Alfonce Nzioka <alfonce.starfish@gmail.com>
2021-04-07 14:38:32 +0300

    remove stray comment

--- src/core/i18n/index.js
+++ src/core/i18n/index.js
@@ -3,12 +3,11 @@
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import i18n from 'i18next';
-
 // $FlowFixMe
 import { initReactI18next } from 'react-i18next';
 
-// NOTE: this is temporary. uncomment when in dev
 import translations from './translations';
+
 import * as LanguageCodes from '../../utils/constants/LanguageCodes';
 
 declare var __ENV_DEV__ :boolean;
@@ -20,7 +19,6 @@ i18n
   .init({
     lng: LanguageCodes.ENGLISH,
     backend: {
-      // loadPath: 'static/i18n/{{lng}}/{{ns}}.json'
       loadPath: (language) => translations[language]
     },
     fallbackLng: LanguageCodes.ENGLISH,
