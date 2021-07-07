1445b68c385a7d325b19ab0f7e7d5e82f9fb5806
Alfonce Nzioka <alfonce.starfish@gmail.com>
2021-07-07 09:25:25 -0700

    use window.location.href to retrieve baseUrl

--- src/containers/study/utils/index.js
+++ src/containers/study/utils/index.js
@@ -1,21 +1,26 @@
 // @flow
 
+const getBaseUrl = () => (
+  window.location.href.split('#')[0]
+);
+
 const getParticipantLoginLink = (orgId :UUID, studyId :UUID, participantId :string) => {
   const rootUrl = 'https://openlattice.com/chronicle/login';
 
   return `${rootUrl}?organizationId=${orgId}&studyId=${studyId}&participantId=${participantId}`;
 };
 
-const getTimeUseDiaryLink = (orgId :UUID, studyId :UUID, participantId :string) => {
-  const rootUrl = 'https://openlattice.com/chronicle/#/time-use-diary';
+const getTimeUseDiaryLink = (orgId :UUID, studyId :UUID, participantId :string) => (
+  `${getBaseUrl()}#/time-use-diary`
+  + `?organizationId=${orgId}`
+  + `&studyId=${studyId}&participantId=${participantId}`
+);
 
-  return `${rootUrl}?organizationId=${orgId}&studyId=${studyId}&participantId=${participantId}`;
-};
-
-const getAppUsageLink = (orgId :UUID, studyId :UUID, participantId :string) => {
-  const rootUrl = 'https://openlattice.com/chronicle/#/survey';
-  return `${rootUrl}?organizationId=${orgId}&studyId=${studyId}&participantId=${participantId}`;
-};
+const getAppUsageLink = (orgId :UUID, studyId :UUID, participantId :string) => (
+  `${getBaseUrl()}#/survey`
+  + `?organizationId=${orgId}`
+  + `&studyId=${studyId}&participantId=${participantId}`
+);
 
 export {
   getAppUsageLink,
