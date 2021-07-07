7b74e72599ccded28f466c2df18ce6cf46995401
Alfonce Nzioka <alfonce.starfish@gmail.com>
2021-07-07 08:07:29 -0700

    add app usage link to participant modal

--- src/containers/study/utils/index.js
+++ src/containers/study/utils/index.js
@@ -1,20 +1,24 @@
 // @flow
 
-const getParticipantLoginLink = (orgId :UUID, studyId :UUID, participantId :UUID) => {
+const getParticipantLoginLink = (orgId :UUID, studyId :UUID, participantId :string) => {
   const rootUrl = 'https://openlattice.com/chronicle/login';
 
   return `${rootUrl}?organizationId=${orgId}&studyId=${studyId}&participantId=${participantId}`;
 };
 
-const getTimeUseDiaryLink = (orgId :UUID, studyId :UUID, participantId :UUID) => {
+const getTimeUseDiaryLink = (orgId :UUID, studyId :UUID, participantId :string) => {
   const rootUrl = 'https://openlattice.com/chronicle/#/time-use-diary';
 
   return `${rootUrl}?organizationId=${orgId}&studyId=${studyId}&participantId=${participantId}`;
 };
 
-/* eslint-disable import/prefer-default-export */
+const getAppUsageLink = (orgId :UUID, studyId :UUID, participantId :string) => {
+  const rootUrl = 'https://openlattice.com/chronicle/#/survey';
+  return `${rootUrl}?organizationId=${orgId}&studyId=${studyId}&participantId=${participantId}`;
+};
+
 export {
+  getAppUsageLink,
   getParticipantLoginLink,
   getTimeUseDiaryLink
 };
-/* eslint-enable */
